4c45498358f5dc9952c67664c24b122b08157fb6
JerryShih <Horizon809@hotmail.com>
2018-03-14 10:21:02 +0800

    Add ShoppingCar Controller Add ShoppingCar Entities

---
+++ MyMvcCourse/Controllers/ShoppingCarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcCourse.Controllers
+{
+    public class ShoppingCarController : Controller
+    {
+        // GET: ShoppingCar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+        }
+
+        public ActionResult Login()
+        {
+
+        }
+    }
+}
